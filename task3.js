15ed00a05d2fc6dae581d1d5238da079b32bf55c
ToSPP <30796779+ToSPP@users.noreply.github.com>
2018-05-30 20:15:35 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -13,13 +13,13 @@ function answer(t1, t2) {
   m  = Math.floor((T - h * 3600) / 60);
   s  = (T - h * 3600) % 60;
   if (h > 0) result += h + " " + addSuffix(h, period[0]);
-  if (h > 0 && m == 0) {
-    result += " " + "0 " + period[1];
-  } else if (m > 0) {
-    result += " " + m + " " + addSuffix(m, period[1]);
+  if (m > 0) result += " " + m + " " + addSuffix(m, period[1]);
+  if (s > 0) {
+    if (h > 0 && m == 0) {
+      result += " " + "0 " + period[1];
+    }
+    result += " " + s + " " + addSuffix(s, period[2]);
   }
-  if (s > 0) result += " " + s + " " + addSuffix(s, period[2]);
-
   return result;
 }
 
@@ -28,9 +28,11 @@ if (t1 >= 1 && t2 <= 100000) {
   }
 
 function addSuffix(num, def) {
-  if (num == 1) {
+  var sNum = String(num);
+  var last = sNum.split("").pop();
+  if (last == 1) {
     def = (def == period[0]) ? def : def + suffix[0];
-  } else if (num > 1 && num < 5) {
+  } else if (last > 1 && last < 5) {
     def = (def == period[0]) ? def + suffix[0] : def + suffix[1];
   } else {
     def = (def == period[0]) ? def + suffix[2] : def;
