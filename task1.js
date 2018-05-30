e2619add14420bafdc5d1edf313614b591d56503
ToSPP <30796779+ToSPP@users.noreply.github.com>
2018-05-30 14:04:02 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -5,8 +5,8 @@ var i = parseFloat(process.argv[2]),
     b = parseInt(process.argv[4]);
 
 var literals = {A: 10, B: 11, C: 12, D: 13, E: 14, F: 15, J: 16, H: 17, I: 18,
-	  			J: 19, K: 20, L: 21, M: 22, N: 23, O: 24, P: 25, Q: 26, R: 27,
-				S: 28, T: 29, U: 30, V: 31, W: 32, X: 33, Y: 34, Z: 35};
+	  	J: 19, K: 20, L: 21, M: 22, N: 23, O: 24, P: 25, Q: 26, R: 27,
+		S: 28, T: 29, U: 30, V: 31, W: 32, X: 33, Y: 34, Z: 35};
 
 function answer(i, a, b) {
   var result, temp;
